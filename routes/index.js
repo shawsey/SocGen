7fac59be4effea9412db482c098b22b8823a8b1e
shawsey <ntshawsey@gmail.com>
2020-05-16 20:09:48 -0400

    Nodejs, expressjs additions

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'SocGEN' });
+});
+
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+
+module.exports = router;
